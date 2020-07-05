c4714c3da70d817f22adc554411a6382dee2b635
michealcoleyoung <coleyoungmusic@gmail.com>
2020-07-05 00:06:51 -0500

    added remaining slides

---
+++ index.js
@@ -0,0 +1,16 @@
+i = 0;
+
+let images = [];
+let time = 3000;
+
+
+images[0] = '..images/slide-1.png';
+images[1] = '..images/slide-2.png';
+images[2] = '..images/slide-3.png';
+images[3] = '..images/slide-4.png';
+images[4] = '..images/slide-5.png';
+images[5] = '..images/slide-6.png';
+images[6] = '..images/slide-7.png';
+images[7] = '..images/slide-8.png';
+
+document.getElementsByClassName('background-image')[0].style.backgroundImage = "url('..images/slide-4.png')";
