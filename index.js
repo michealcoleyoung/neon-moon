f1c85a16087ccb0eb6ee1d4690b6b6b6c023466c
michealcoleyoung <coleyoungmusic@gmail.com>
2020-07-05 20:11:16 -0500

    started slideshow

--- index.js
+++ index.js
@@ -4,13 +4,32 @@ let images = [];
 let time = 3000;
 
 
-images[0] = '..images/slide-1.png';
-images[1] = '..images/slide-2.png';
-images[2] = '..images/slide-3.png';
-images[3] = '..images/slide-4.png';
-images[4] = '..images/slide-5.png';
-images[5] = '..images/slide-6.png';
-images[6] = '..images/slide-7.png';
-images[7] = '..images/slide-8.png';
+images[0] = 'images/slide-1.png';
+images[1] = 'images/slide-2.png';
+images[2] = 'images/slide-3.png';
+images[3] = 'images/slide-4.png';
+images[4] = 'images/slide-5.png';
+images[5] = 'images/slide-6.png';
+images[6] = 'images/slide-7.png';
+images[7] = 'images/slide-8.png';
 
-document.getElementsByClassName('background-image')[0].style.backgroundImage = "url('..images/slide-4.png')";
+
+
+
+function changeImg() {
+    document.slide.src = images[i];
+    let img = document.getElementById("images").src;
+    console.log(img);
+
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout("changeImg()", time);
+
+}
+
+
+window.onload = changeImg;
