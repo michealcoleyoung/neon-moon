5db03d02c7d2892779e3a0c8b72c06333e31004f
michealcoleyoung <coleyoungmusic@gmail.com>
2020-07-14 21:43:06 -0500

    added set setSlides function and updated setButtonColor functionality

--- index.js
+++ index.js
@@ -15,47 +15,68 @@ images[5] = 'images/slide-6.png';
 images[6] = 'images/slide-7.png';
 images[7] = 'images/slide-8.png';
 
-
-function setColor() {
+// sets CREATE button color that corresponds to given image
+function setButtonColor() {
     let img = document.getElementById("images").src;
     let button = document.getElementById('button');
+    let slide = document.getElementsByClassName('slides');
 
+    button.classList.remove('hover-class1', 'hover-class2',
+        'hover-class3', 'hover-class4', 'hover-class5',
+        'hover-class6', 'hover-class7',
+        'hover-class8');
 
     switch (true) {
         case (img == `${host}${images[0]}`):
-            button.classList.remove('hover-class1', 'hover-class2');
+            slide[0].style = "background-color: #2EBAFF";
             button.classList.add('hover-class1');
             break;
         case (img == `${host}${images[1]}`):
-            button.classList.remove('hover-class1', 'hover-class2');
+            slide[1].style = "background-color: #2EBAFF";
             button.classList.add('hover-class2');
             break;
         case (img == `${host}${images[2]}`):
-            document.getElementById("button").style.color = "red";
+            slide[2].style = "background-color: #2EBAFF";
+            button.classList.add('hover-class3');
             break;
         case (img == `${host}${images[3]}`):
-            document.getElementById("button").style.color = "purple";
+            slide[3].style = "background-color: #2EBAFF";
+            button.classList.add('hover-class4');
             break;
         case (img == `${host}${images[4]}`):
-            document.getElementById("button").style.color = "blue";
+            slide[4].style = "background-color: #2EBAFF";
+            button.classList.add('hover-class5');
             break;
         case (img == `${host}${images[5]}`):
-            document.getElementById("button").style.color = "blue";
+            slide[5].style = "background-color: #2EBAFF";
+            button.classList.add('hover-class6');
             break;
         case (img == `${host}${images[6]}`):
-            document.getElementById("button").style.color = "blue";
+            slide[6].style = "background-color: #2EBAFF";
+            button.classList.add('hover-class7');
             break;
         case (img == `${host}${images[7]}`):
-            document.getElementById("button").style.color = "blue";
+            slide[7].style = "background-color: #2EBAFF";
+            button.classList.add('hover-class8');
             break;
+    }
+
+}
+
+// changes slide dot to #2EBAFF when on given image
+function setSlides() {
+    let slides = document.getElementsByClassName('slides');
+    for (let slide of slides) {
+        slide.style = "background-color: white";
     }
 }
 
 
+// starts image slideshow
 function changeImg() {
     document.slide.src = images[i];
-    setColor();
-
+    setSlides();
+    setButtonColor();
     if (i < images.length - 1) {
         i++;
     } else {
